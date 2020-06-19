564373fd92675ef388e57ecb0606859913cb9b66
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2020-06-18 20:59:13 -0700

    Updated /Remote
    Fixed file name typos; changed from '-' to '_'

--- CloudFormation/Remote/bastion-login.sh
+++ CloudFormation/Remote/bastion-login.sh
@@ -6,7 +6,7 @@ chmod 400 bastion-key.pem
 echo $'Copying the Jenkins Key and Login Script...\n'
 scp -i bastion-key.pem spark-key.pem ubuntu@$BIP:/home/ubuntu/spark-key.pem
 scp -i bastion-key.pem $PROJ/spark-login.sh ubuntu@$BIP:/home/ubuntu/spark-login.sh
-scp -i bastion-key.pem $PROJ/bootstrap-emr.sh ubuntu@$BIP:/home/ubuntu/bootstrap-emr.sh
+scp -i bastion-key.pem $PROJ/bootstrap_emr.sh ubuntu@$BIP:/home/ubuntu/bootstrap_emr.sh
 scp -i bastion-key.pem $PROJ/sparkify_log_small.json ubuntu@$BIP:/home/ubuntu/sparkify_log_small.json
 echo $'Logging Into Secure Shell...\n'
 ssh -i bastion-key.pem ubuntu@$BIP
--- CloudFormation/Remote/spark-login.sh
+++ CloudFormation/Remote/spark-login.sh
@@ -3,5 +3,5 @@ echo "Enter the Spark Master's Private IP Address:"
 read SIP
 chmod 400 spark-key.pem
 scp -i spark-key.pem sparkify_log_small.json hadoop@$SIP:/home/hadoop/sparkify_log_small.json
-scp -i spark-key.pem bootstrap-emr.sh hadoop@$SIP:/home/hadoop/bootstrap-emr.sh
+scp -i spark-key.pem bootstrap_emr.sh hadoop@$SIP:/home/hadoop/bootstrap_emr.sh
 ssh -i spark-key.pem hadoop@$SIP
